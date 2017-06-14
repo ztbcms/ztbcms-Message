a1046bb967591e18ee98d2975b9fcb63a552a008
Jayin Ton <tonjayin@gmail.com>
2017-06-14 19:35:17 +0800

    改进消息后台配置

--- Install/Menu.php
+++ Install/Menu.php
@@ -3,7 +3,7 @@
 return array(
     array(
         //父菜单ID，NULL或者不写系统默认，0为顶级菜单
-        "parentid" => null,
+        "parentid" => 37,
         //地址，[模块/]控制器/方法
         "route" => "Message/Message/index",
         //类型，1：权限认证+菜单，0：只作为菜单
@@ -11,9 +11,9 @@ return array(
         //状态，1是显示，0不显示（需要参数的，建议不显示，例如编辑,删除等操作）
         "status" => 1,
         //名称
-        "name" => "消息",
+        "name" => "消息管理",
         //备注
-        "remark" => "消息相关",
+        "remark" => "",
         //子菜单列表
         "child" => array(
             array(
